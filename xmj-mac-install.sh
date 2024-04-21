32ba2252c4929585f027f3be41eca0b7961969af
Squizzy <crazy_squirrel@yahoo.com>
2024-04-21 16:01:40 +0100

    First commit

---
+++ xmj-mac-install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Creare a folder for the setup
+mkdir XMJ-MacOS-Install
+
+# Go to this folder
+cd XMJ-MacOS-Install
+
+# Download XMJ Mahjong source code from its original website (from Julian Bradfield)
+wget https://mahjong.julianbradfield.org/Source/mj-1.16-src.tar.gz
+
+# Unzip the downloaded file
+unzip ./mj-1.16-src.tar.gz
+
