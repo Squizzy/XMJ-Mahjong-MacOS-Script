ae2fa3ddd01cceef90754c987c9269fab1324df4
Squizzy <crazy_squirrel@yahoo.com>
2025-08-04 18:07:47 +0200

    Small updates to the files used for patching; added controller.c

--- patches/xmj_1.17/create_patches.sh
+++ patches/xmj_1.17/create_patches.sh
@@ -5,7 +5,10 @@ VERSION="1.17"
 # greedy_xmj_${VERSION}.c is the renamed copy of the original greedy.c for the ${VERSION} version.
 diff -u xmj_${VERSION}/greedy_xmj_${VERSION}.c xmj_${VERSION}/greedy_xmj_${VERSION}_fixed_for_socket_port.c > greedy_xmj_${VERSION}_socket_port_fix.patch
 
+# controller_xmj_${VERSION}.c is the renamed copy of the original controller.c for the ${VERSION} version.
+diff -u xmj_${VERSION}/controller_xmj_${VERSION}.c xmj_${VERSION}/controller_xmj_${VERSION}_fixed_for_socket_port.c > controller_xmj_${VERSION}_socket_port_fix.patch
+
 # gui_xmj_${VERSION}.c is the renamed copy of the original gui.c for the ${VERSION} version.
 diff -u xmj_${VERSION}/gui_xmj_${VERSION}.c xmj_${VERSION}/gui_xmj_${VERSION}_fixed_for_socket_port.c > gui_xmj_${VERSION}_socket_port_fix.patch
 diff -u xmj_${VERSION}/gui_xmj_${VERSION}.c xmj_${VERSION}/gui_xmj_${VERSION}_fixed_for_executables_relative_path.c > gui_xmj_${VERSION}_executables_relative_path_fix.patch
-diff -u xmj_${VERSION}/gui_xmj_${VERSION}.c xmj_${VERSION}/gui_xmj_${VERSION}_fixed_for_tiles_background.c > gui_xmj_${VERSION}_tiles_blackground_fix.patch
+diff -u xmj_${VERSION}/gui_xmj_${VERSION}.c xmj_${VERSION}/gui_xmj_${VERSION}_fixed_for_tiles_display.c > gui_xmj_${VERSION}_tiles_display_fix.patch
