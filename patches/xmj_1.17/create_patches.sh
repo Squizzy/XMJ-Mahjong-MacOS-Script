861ab33b013bda98eb505b7ebdaba7c8871363e6
Squizzy <crazy_squirrel@yahoo.com>
2025-08-10 13:10:09 +0200

    removed now obsolete patch for executables location

--- patches/xmj_1.17/create_patches.sh
+++ patches/xmj_1.17/create_patches.sh
@@ -10,5 +10,7 @@ diff -u xmj_${VERSION}/controller_xmj_${VERSION}.c xmj_${VERSION}/controller_xmj
 
 # gui_xmj_${VERSION}.c is the renamed copy of the original gui.c for the ${VERSION} version.
 diff -u xmj_${VERSION}/gui_xmj_${VERSION}.c xmj_${VERSION}/gui_xmj_${VERSION}_fixed_for_socket_port.c > gui_xmj_${VERSION}_socket_port_fix.patch
-diff -u xmj_${VERSION}/gui_xmj_${VERSION}.c xmj_${VERSION}/gui_xmj_${VERSION}_fixed_for_executables_relative_path.c > gui_xmj_${VERSION}_executables_relative_path_fix.patch
 diff -u xmj_${VERSION}/gui_xmj_${VERSION}.c xmj_${VERSION}/gui_xmj_${VERSION}_fixed_for_tiles_display.c > gui_xmj_${VERSION}_tiles_display_fix.patch
+
+# Now obsolete
+# diff -u xmj_${VERSION}/gui_xmj_${VERSION}.c xmj_${VERSION}/gui_xmj_${VERSION}_fixed_for_executables_relative_path.c > gui_xmj_${VERSION}_executables_relative_path_fix.patch
